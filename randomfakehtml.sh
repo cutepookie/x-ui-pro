c67c2ed6d65f423f7cbe3b1f654d6c2a9222f730
Xue Xianliang 自由的世界 <146182012+GFW4Fun@users.noreply.github.com>
2023-09-28 12:24:41 -0700

    Update randomfakehtml.sh

--- randomfakehtml.sh
+++ randomfakehtml.sh
@@ -1,4 +1,25 @@
-echo "comming soon"
-exit 1
-
-#https://github.com/GFW4Fun/randomfakehtml/archive/refs/heads/master.zip
+#!/bin/bash
+### https://github.com/GFW4Fun
+apt update && apt install unzip -y
+cd $HOME
+if [[ -d "randomfakehtml-master" ]]; then
+	cd randomfakehtml-master
+else
+	wget https://github.com/GFW4Fun/randomfakehtml/archive/refs/heads/master.zip
+	unzip master.zip && rm master.zip
+	cd randomfakehtml-master
+	rm -rf assets
+	rm .gitattributes README.md _config.yml
+fi
+###################################
+#RandomHTML=$(for i in *; do echo "$i"; done | shuf -n1 2>&1)
+RandomHTML=$(a=(*); echo ${a[$((RANDOM % ${#a[@]}))]} 2>&1)
+echo "Template: ${RandomHTML}"
+#################################
+if [[ -d ${RandomHTML} && -d "/var/www/html/" ]]; then
+	rm -rf /var/www/html/*
+	cp -a ${RandomHTML}/. "/var/www/html/"
+fi
+####################################
+echo "Done."
+############################
