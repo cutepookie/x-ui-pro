67b92bf7eb239dbe4b0afaeec6bf6f7a82d643c0
Xue Xianliang 自由的世界 <146182012+GFW4Fun@users.noreply.github.com>
2023-09-28 12:34:09 -0700

    Update randomfakehtml.sh

--- randomfakehtml.sh
+++ randomfakehtml.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 ### https://github.com/GFW4Fun
+
+Green="\033[32m"
+Red="\033[31m"
+Yellow="\033[33m"
+Blue="\033[36m"
+Font="\033[0m"
+OK="${Green}[OK]${Font}"
+ERROR="${Red}[ERROR]${Font}"
+function msg_inf() {  echo -e "${Blue} $1 ${Font}"; }
+function msg_ok() { echo -e "${OK} ${Blue} $1 ${Font}"; }
+function msg_err() { echo -e "${ERROR} ${Yellow} $1 ${Font}"; }
+###################################
 apt update && apt install unzip -y
 cd $HOME
 if [[ -d "randomfakehtml-master" ]]; then
@@ -14,12 +26,12 @@ fi
 ###################################
 #RandomHTML=$(for i in *; do echo "$i"; done | shuf -n1 2>&1)
 RandomHTML=$(a=(*); echo ${a[$((RANDOM % ${#a[@]}))]} 2>&1)
-echo "Template: ${RandomHTML}"
+msg_inf "Random template name: ${RandomHTML}"
 #################################
 if [[ -d ${RandomHTML} && -d "/var/www/html/" ]]; then
 	rm -rf /var/www/html/*
 	cp -a ${RandomHTML}/. "/var/www/html/"
+	msg_ok "Template extracted successfully!"
+else
+	msg_err "Extraction error!"
 fi
-####################################
-echo "Done."
-############################
